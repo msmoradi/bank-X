faf3055dc7a1d9cf3e4ececa037494a08df66ece
msmoradi <mr.ms.moradi@gmail.com>
2023-12-12 14:52:14 +0330

    add core theme colors and text styles

---
+++ lib/core/designsystem/theme/input_decoration_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+class BanxInputDecorationTheme {
+  static InputDecorationTheme inputDecorationTheme = const InputDecorationTheme(
+    isDense: true,
+    border: OutlineInputBorder(),
+  );
+}
---
+++ lib/core/designsystem/theme/text_styles.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class FalomenTextStyles {
+  static const fontFamilyName = 'IRANSansXFaNum';
+
+  static TextTheme textTheme = const TextTheme(
+    displayLarge: TextStyle(
+      fontSize: 57,
+      fontWeight: FontWeight.bold,
+    ),
+    displayMedium: TextStyle(
+      fontSize: 45,
+      fontWeight: FontWeight.bold,
+    ),
+    displaySmall: TextStyle(
+      fontSize: 36,
+      fontWeight: FontWeight.normal,
+    ),
+    headlineLarge: TextStyle(
+      fontSize: 32,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineMedium: TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineSmall: TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+    ),
+    titleLarge: TextStyle(
+      fontSize: 22,
+      fontWeight: FontWeight.normal,
+    ),
+    titleMedium: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.normal,
+    ),
+    titleSmall: TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.normal,
+    ),
+    bodyLarge: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.normal,
+    ),
+    bodyMedium: TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.normal,
+    ),
+    bodySmall: TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.normal,
+    ),
+    labelLarge: TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w600,
+    ),
+    labelMedium: TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.bold,
+    ),
+    labelSmall: TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w500,
+    ),
+  );
+}
