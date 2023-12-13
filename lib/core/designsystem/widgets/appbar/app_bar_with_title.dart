8fb81ac699e97973be245ec323739ff581a13a1a
msmoradi <mr.ms.moradi@gmail.com>
2023-12-13 09:34:07 +0330

    create appbar widgets

---
+++ lib/core/designsystem/widgets/appbar/app_bar_with_title.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppBarWithTitle extends StatelessWidget {
+  final String title;
+
+  const AppBarWithTitle({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 16.0,
+      ),
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: Text(
+          style: Theme.of(context)
+              .textTheme
+              .headlineMedium,
+          title,
+        ),
+      ),
+    );
+  }
+}
