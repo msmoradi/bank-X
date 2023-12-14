cd89088a6a9f5cc0732c31bcde38505ceaad0986
msmoradi <mr.ms.moradi@gmail.com>
2023-12-14 09:47:28 +0330

    create text field widgets

---
+++ lib/core/designsystem/widgets/textfields/phone_number_text_field.dart
@@ -0,0 +1,45 @@
+import 'package:banx/core/utils/extension/build_context.dart';
+import 'package:flutter/material.dart';
+
+import '../../../utils/validators/phone_number_validator.dart';
+
+class PhoneNumberTextField extends StatefulWidget {
+  final FormFieldSetter<String>? onSaved;
+  final ValueChanged<String>? onFieldSubmitted;
+  final TextEditingController? controller;
+  final FocusNode? focusNode;
+  final bool autofocus;
+
+  const PhoneNumberTextField({
+    super.key,
+    this.onSaved,
+    this.onFieldSubmitted,
+    this.controller,
+    this.focusNode,
+    this.autofocus = false,
+  });
+
+  @override
+  State<PhoneNumberTextField> createState() => _PhoneNumberTextFieldState();
+}
+
+class _PhoneNumberTextFieldState extends State<PhoneNumberTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      autofocus: widget.autofocus,
+      focusNode: widget.focusNode,
+      controller: widget.controller,
+      onSaved: widget.onSaved,
+      onFieldSubmitted: widget.onFieldSubmitted,
+      keyboardType: TextInputType.number,
+      decoration: const InputDecoration(
+        prefixIcon: Icon(Icons.phone_iphone),
+        labelText: "شماره تلفن همراه",
+      ),
+      validator: (value) {
+        return context.validateFiled(PhoneNumberValidator(value));
+      },
+    );
+  }
+}
---
+++ lib/core/designsystem/widgets/textfields/rounded_with_shadow_otp.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
+
+class RoundedWithShadowInput extends StatefulWidget {
+  final int length;
+  final TextEditingController? controller;
+  final FocusNode? focusNode;
+  final ValueChanged<String>? onCompleted;
+  final ValueChanged<String>? onChanged;
+  final ValueChanged<String>? onSubmitted;
+  final FormFieldValidator<String>? validator;
+  final bool obscureText;
+  final bool useNativeKeyboard;
+  final bool autofocus;
+  final bool readOnly;
+  final String? errorText;
+  final bool forceErrorState;
+  final SmsRetriever? smsRetriever;
+  final double? separatorWidth;
+
+  const RoundedWithShadowInput({
+    super.key,
+    this.controller,
+    this.focusNode,
+    this.length = 4,
+    this.onCompleted,
+    this.onChanged,
+    this.onSubmitted,
+    this.validator,
+    this.obscureText = false,
+    this.useNativeKeyboard = true,
+    this.autofocus = true,
+    this.readOnly = false,
+    this.errorText,
+    this.forceErrorState = false,
+    this.smsRetriever,
+    this.separatorWidth,
+  });
+
+  @override
+  _RoundedWithShadowInputState createState() => _RoundedWithShadowInputState();
+
+  @override
+  String toStringShort() => 'Rounded With Shadow';
+}
+
+class _RoundedWithShadowInputState extends State<RoundedWithShadowInput> {
+  @override
+  Widget build(BuildContext context) {
+    final defaultPinTheme = PinTheme(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(color: Theme.of(context).colorScheme.outline),
+        color: Theme.of(context).colorScheme.surface,
+      ),
+    );
+
+    return Pinput(
+      length: widget.length,
+      smsRetriever: widget.smsRetriever,
+      controller: widget.controller,
+      readOnly: widget.readOnly,
+      focusNode: widget.focusNode,
+      onCompleted: widget.onCompleted,
+      onChanged: widget.onChanged,
+      autofocus: widget.autofocus,
+      onSubmitted: widget.onSubmitted,
+      defaultPinTheme: defaultPinTheme,
+      validator: widget.validator,
+      errorText: widget.errorText,
+      errorBuilder: (String? errorText, String pin) {
+        return Align(
+          alignment: Alignment.center,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: Text(
+              errorText ?? 'خطایی رخ داده است',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodySmall
+                  ?.copyWith(color: Theme.of(context).colorScheme.error),
+            ),
+          ),
+        );
+      },
+      forceErrorState: widget.forceErrorState,
+      obscureText: widget.obscureText,
+      pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
+      obscuringWidget: Container(
+        width: 10,
+        height: 10,
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Theme.of(context).colorScheme.primary),
+      ),
+      useNativeKeyboard: widget.useNativeKeyboard,
+      hapticFeedbackType: HapticFeedbackType.lightImpact,
+      separatorBuilder: (index) => widget.length > 4
+          ? const SizedBox(width: 12)
+          : const SizedBox(width: 16),
+      focusedPinTheme: defaultPinTheme.copyBorderWith(
+        border: Border.all(
+          width: 2,
+          color: Theme.of(context).colorScheme.outline,
+        ),
+      ),
+      errorPinTheme: defaultPinTheme.copyBorderWith(
+        border: Border.all(color: Theme.of(context).colorScheme.error),
+      ),
+      showCursor: false,
+    );
+  }
+}
