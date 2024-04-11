c1edb1669dac495c88d35577379508d3bd96b196
msmoradi <mr.ms.moradi@gmail.com>
2024-04-11 09:54:07 +0330

    create transaction destination page

---
+++ lib/composition/transaction_destination_page_factory.dart
@@ -0,0 +1,59 @@
+import 'package:banx/composition/transaction_checkout_page_factory.dart';
+import 'package:banx/core/designsystem/widgets/components/drop_down_chip_widget.dart';
+import 'package:banx/feature/transaction_destination/presentation/view/transaction_destination_content.dart';
+import 'package:banx/feature/transaction_destination/presentation/view/transaction_destination_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:go_router/go_router.dart';
+
+class TransactionDestinationPageFactory {
+  static const path = "/transaction_destination";
+
+  static TransactionDestinationPage builder(
+      {required BuildContext context,
+      required TransactionDestinationExtra extra,
+      required Function(String message) showMessage}) {
+    return TransactionDestinationPage(
+      showMessage: showMessage,
+      amount: extra.amount,
+      sourceDepositType: extra.sourceDepositType,
+      onDestinationSelected: (userInfo) {
+        context.push(TransactionCheckoutPageFactory.path,
+            extra: TransactionCheckoutExtra(
+                amount: extra.amount,
+                sourceDepositType: extra.sourceDepositType,
+                sourceUserInfo: UserInfo(
+                  name: 'کاربر نمونه',
+                  cardNumber: '0000000000000000',
+                  photoUrl: 'https://i.pravatar.cc/150',
+                ),
+                destinationDepositType: DepositType.rialDeposit,
+                destinationUserInfo: userInfo));
+      },
+    );
+  }
+
+  static GoRoute route({
+    List<RouteBase> routes = const <RouteBase>[],
+    required Function(String message) showMessage,
+  }) {
+    return GoRoute(
+        path: (TransactionDestinationPageFactory.path),
+        builder: (ctx, state) {
+          TransactionDestinationExtra extra =
+              state.extra as TransactionDestinationExtra;
+          return TransactionDestinationPageFactory.builder(
+              context: ctx, showMessage: showMessage, extra: extra);
+        },
+        routes: routes);
+  }
+}
+
+class TransactionDestinationExtra {
+  final String amount;
+  final DepositType sourceDepositType;
+
+  TransactionDestinationExtra({
+    required this.amount,
+    required this.sourceDepositType,
+  });
+}
---
+++ lib/feature/transaction_destination/presentation/view/transaction_destination_content.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:persian_number_utility/persian_number_utility.dart';
+
+class TransactionDestinationContent extends StatefulWidget {
+  final String amount;
+  final Function(UserInfo) onDestinationSelected;
+
+  const TransactionDestinationContent({
+    super.key,
+    required this.amount,
+    required this.onDestinationSelected,
+  });
+
+  @override
+  State<TransactionDestinationContent> createState() =>
+      _TransactionDestinationContentState();
+}
+
+class _TransactionDestinationContentState
+    extends State<TransactionDestinationContent> {
+  late TextEditingController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        forceMaterialTransparency: true,
+        surfaceTintColor: Colors.transparent,
+        leading: const BackButton(),
+        centerTitle: false,
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              width: 32,
+              height: 32,
+              'assets/icons/search.svg',
+              colorFilter: ColorFilter.mode(
+                Theme.of(context).colorScheme.primary,
+                BlendMode.srcIn,
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              width: 32,
+              height: 32,
+              'assets/icons/help-circle.svg',
+              colorFilter: ColorFilter.mode(
+                Theme.of(context).colorScheme.primary,
+                BlendMode.srcIn,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  'مقصد انتقال',
+                  style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+              ),
+              const SizedBox(height: 12),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  'انتقال ${widget.amount} به',
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                      color: Theme.of(context).colorScheme.onSurfaceVariant),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: controller,
+                autofocus: false,
+                maxLines: 1,
+                keyboardType: TextInputType.text,
+                decoration:
+                    const InputDecoration(labelText: "کارت، شبا، نام‌، تلفن"),
+              ),
+              const SizedBox(height: 16),
+              DestinationRow(
+                userInfo: UserInfo(
+                  name: 'کاربر ۱',
+                  cardNumber: '0000000000000001',
+                  photoUrl: 'https://i.pravatar.cc/150',
+                ),
+                onTap: widget.onDestinationSelected,
+              ),
+              DestinationRow(
+                userInfo: UserInfo(
+                  name: 'کاربر ۲',
+                  cardNumber: '0000000000000002',
+                  photoUrl: 'https://i.pravatar.cc/151',
+                ),
+                onTap: widget.onDestinationSelected,
+              ),
+              DestinationRow(
+                userInfo: UserInfo(
+                  name: 'کاربر ۳',
+                  cardNumber: '0000000000000003',
+                  photoUrl: 'https://i.pravatar.cc/152',
+                ),
+                onTap: widget.onDestinationSelected,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class UserInfo {
+  final String name;
+  final String cardNumber;
+  final String photoUrl;
+
+  UserInfo(
+      {required this.name, required this.cardNumber, required this.photoUrl});
+}
+
+class DestinationRow extends StatelessWidget {
+  final UserInfo userInfo;
+  final Function(UserInfo) onTap;
+
+  const DestinationRow(
+      {super.key, required this.userInfo, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        onTap(userInfo);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12.0),
+        child: UserInfoWidget(
+          userInfo: userInfo,
+        ),
+      ),
+    );
+  }
+}
+
+class UserInfoWidget extends StatelessWidget {
+  final UserInfo userInfo;
+
+  const UserInfoWidget({super.key, required this.userInfo});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Stack(alignment: Alignment.bottomLeft, children: [
+          CircleAvatar(
+            radius: 20.0,
+            backgroundImage: NetworkImage(userInfo.photoUrl),
+          ),
+          CircleAvatar(
+            radius: 7.0,
+            backgroundColor: Theme.of(context).colorScheme.surface,
+            backgroundImage: const AssetImage('assets/images/x1.png'),
+          ),
+        ]),
+        const SizedBox(
+          width: 12,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              userInfo.name,
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Text(
+              textDirection: TextDirection.ltr,
+              userInfo.cardNumber.charRagham(separator: " ").toPersianDigit(),
+              style: Theme.of(context).textTheme.bodySmall,
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
